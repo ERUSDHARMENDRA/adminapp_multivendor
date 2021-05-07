0afbbccbb8eb3dd21ac7fa9a86160d47cdf7a54d
Dharmendra Mehta <dharmendramehta622@gmail.com>
2021-05-07 19:19:18 +0545

    'Admin login form designed with no errors'

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shapeyou_admin/screens/login_screen.dart';
+
 
 void main(){
   runApp(MyApp());
@@ -11,149 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shop4ShapeYou Admin Dash Board',
+      title: 'Shop4ShapeYou Admin DashBoard',
       theme: ThemeData(
         primaryColor: Color(0xFF995F06),
       ),
-      home: MyHomePage(title: 'ShapeYou App Admin Dashboard',),
+      home: LoginScreen(),
     );
   }
 }
 
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({ Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _formKey,
-      appBar: AppBar(
-        title: Text(widget.title, style: TextStyle(color: Colors.white),),
-        elevation: 0.0,
-        centerTitle: true,
-      ),
-      body: Container(
-decoration: BoxDecoration(
-  gradient: LinearGradient(
-    colors: [
-      Color(0xFF995F06),
-      Colors.white
-    ],
-      stops: [1.0,1.0],
-      begin: Alignment.topCenter,
-      end: Alignment(0,0),
-  ),
-),
-
-        child: Center(
-          child: Container(
-            height: 400,
-            width: 300,
-            child: Card(
-              elevation:6,
-              shape: Border.all(color: Theme.of(context).primaryColor, width: 2),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Container(
-                          child: Column(
-                            children: [
-                              Image.asset('images/logo.png', height: 92,width: 92,),
-                              Text('Shop4ShapeYou App Admin', style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900, fontSize:20),),
-                              TextFormField(
-                                validator: (value){
-                                  if (value!.isEmpty) {
-                                    return 'Enter a User Name';
-                                  }
-                                  return null;
-                                },
-                                decoration: InputDecoration(
-                                  labelText: 'User Name',
-                                  prefixIcon:Icon(Icons.person),
-                                  focusColor:Theme.of(context).primaryColor,
-                                  contentPadding: EdgeInsets.only(left: 20,right: 20),
-                                  hintText:'User Name',
-                                  border: OutlineInputBorder(),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                      color: Theme.of(context).primaryColor,
-                                        width: 2,
-                                    )
-                                  ),
-                                ),
-                              ),
-
-                              SizedBox(height: 20,),
-
-                              TextFormField(
-                                validator: (value){
-                                  if (value!.isEmpty) {
-                                    return 'Enter a Password';
-                                  }
-                                  if (value.length<6) {
-                                    return 'Minimun 6 Characters';
-                                  }
-                                  return null;
-                                },
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  labelText: 'Minimun 6 Characters',
-                                  prefixIcon: Icon(Icons.vpn_key_rounded),
-                                  focusColor:Theme.of(context).primaryColor,
-                                  contentPadding: EdgeInsets.only(left: 20,right: 20),
-                                  hintText:'Password',
-                                  border: OutlineInputBorder(),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor,
-                                        width: 2,
-                                      )
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-
-                      Row(
-                        children: [
-                          Expanded(
-                            child: FlatButton(
-                              onPressed: () { 
-                                if (_formKey.currentState!.validate()) {
-                                  print('Validated');
-                                }  
-                              },
-                              color: Theme.of(context).primaryColor,
-                              child: Text('Login',style: TextStyle(color: Colors.white),),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,234 @@
+import 'package:ars_dialog/ars_dialog.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shapeyou_admin/services/firebase_services.dart';
+
+class LoginScreen extends StatefulWidget {
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  final _formKey = GlobalKey<FormState>();
+  FirebaseServices _services = FirebaseServices();
+var _usernameTextController = TextEditingController();
+  var _passwordTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    ProgressDialog progressDialog = ProgressDialog(context,
+        message: Text("Target Inbound"),
+        title: Text("HummingBird approaching.ETA 1 minutes")
+    );
+
+
+    Future<void> _showMyDialog({title, message}) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext dialogContext) {
+          return AlertDialog(
+            title: Text(title),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text(message),
+                  Text('Please try again'),
+                ],
+              ),),
+            actions: <Widget>[
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+
+
+
+
+    _login({username, password}) async {
+    progressDialog.show();
+    _services.getAdminCredentials(username).then((value){
+     if(value.exists){
+       if(value.data()['username']==username){
+        if(value.data()['password']==password){
+
+        }
+        return ;
+       }
+       progressDialog.dismiss();
+       _showMyDialog(title: 'Login Failed', message: 'Incorrect Username or Password');
+     }
+    });
+  }
+
+
+    return Scaffold(
+          key: _formKey,
+          body: FutureBuilder(
+            // Initialize FlutterFire:
+            future: _initialization,
+            builder: (context, snapshot) {
+              // Check for errors
+              if (snapshot.hasError) {
+                return Center(child: Text('Connection Failed'),);
+              }
+
+              // Once complete, show your application
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF995F06),
+                        Colors.white
+                      ],
+                      stops: [1.0, 1.0],
+                      begin: Alignment.topCenter,
+                      end: Alignment(0, 0),
+                    ),
+                  ),
+
+                  child: Center(
+                    child: Container(
+                      height: 420,
+                      width: 300,
+                      child: Card(
+                        elevation: 6,
+                        shape: Border.all(color: Theme
+                            .of(context)
+                            .primaryColor, width: 2),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: Container(
+                                    child: Column(
+                                      children: [
+                                        Image.asset(
+                                          'images/logo.png', height: 92,
+                                          width: 92,),
+                                        Text('Shop4ShapeYou App Admin',
+                                          style: TextStyle(color: Colors.white,
+                                              fontWeight: FontWeight.w900,
+                                              fontSize: 20),),
+                                        TextFormField(
+                                          controller: _usernameTextController,
+                                          validator: (value) {
+                                            if (value!.isEmpty) {
+                                              return 'Enter a User Name';
+                                            }
+                                            return null;
+                                          },
+                                          decoration: InputDecoration(
+                                            labelText: 'User Name',
+                                            prefixIcon: Icon(Icons.person),
+                                            focusColor: Theme
+                                                .of(context)
+                                                .primaryColor,
+                                            contentPadding: EdgeInsets.only(
+                                                left: 20, right: 20),
+                                            hintText: 'User Name',
+                                            border: OutlineInputBorder(),
+                                            focusedBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                  color: Theme
+                                                      .of(context)
+                                                      .primaryColor,
+                                                  width: 2,
+                                                )
+                                            ),
+                                          ),
+                                        ),
+
+                                        SizedBox(height: 20,),
+
+                                        TextFormField(
+                                          controller: _passwordTextController,
+                                          validator: (value) {
+                                            if (value!.isEmpty) {
+                                              return 'Enter a Password';
+                                            }
+                                            if (value.length < 6) {
+                                              return 'Minimun 6 Characters';
+                                            }
+                                            return null;
+                                          },
+                                          obscureText: true,
+                                          decoration: InputDecoration(
+                                            labelText: 'Minimun 6 Characters',
+                                            prefixIcon: Icon(
+                                                Icons.vpn_key_rounded),
+                                            focusColor: Theme
+                                                .of(context)
+                                                .primaryColor,
+                                            contentPadding: EdgeInsets.only(
+                                                left: 20, right: 20),
+                                            hintText: 'Password',
+                                            border: OutlineInputBorder(),
+                                            focusedBorder: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                  color: Theme
+                                                      .of(context)
+                                                      .primaryColor,
+                                                  width: 2,
+                                                )
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: FlatButton(
+                                        onPressed: () {
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                          }
+                                        },
+                                        color: Theme
+                                            .of(context)
+                                            .primaryColor,
+                                        child: Text('Login', style: TextStyle(
+                                            color: Colors.white),),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }
+
+              // Otherwise, show something whilst waiting for initialization to complete
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        );
+      }
+}
