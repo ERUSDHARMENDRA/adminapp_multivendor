c7a3a34d6d6f1e3734fcd39625dab6c9ff330b70
Dharmendra Mehta <dharmendramehta622@gmail.com>
2021-06-11 21:57:13 +0545

    'Admin login form designed with no errors'

---
+++ android/app/src/main/java/com/example/shapeyou_admin/MainActivity.java
@@ -0,0 +1,6 @@
+package com.webdlabs.shapeyou_admin;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shapeyou_admin/screens/home_screen.dart';
 import 'login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
